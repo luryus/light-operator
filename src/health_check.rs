566f1796d02e029f48f611e200d94f56c23ad395
Lauri Koskela <luryus@users.noreply.github.com>
2023-11-29 20:18:55 +0200

    Dependency updates

--- src/health_check.rs
+++ src/health_check.rs
@@ -1,7 +1,7 @@
 use std::{net::SocketAddr, sync::Arc};
 
 use anyhow::{Context, Error};
-use axum::{routing::get, Router, Server};
+use axum::{routing::get, Router};
 
 use crate::config::Config;
 
@@ -9,8 +9,8 @@ pub async fn run(config: Arc<Config>) -> Result<(), Error> {
     let app = Router::new().route("/healthz", get(())); // Always succeed
 
     let addr = SocketAddr::from(([0, 0, 0, 0], config.health_check.port));
-    Server::try_bind(&addr)?
-        .serve(app.into_make_service())
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    axum::serve(listener, app)
         .await
         .context("Health check server failed")
 }
